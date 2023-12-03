9fb7cb57eff96f2600c58a57c028ad190b2b8cd8
edobrugnii <147245682+edobrugnii@users.noreply.github.com>
2023-12-03 15:54:14 +0000

    no password

--- esercizi/questionario/index.php
+++ esercizi/questionario/index.php
@@ -1,35 +1,37 @@
+<!DOCTYPE html>
 <html>
+<body>
 
-<head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-</head>
-<?php
-    echo "<body style=\"background-color: $_GET[favcolor];\">"
-    ?>
+<h2>Registrati</h2>
 
-    <div class="container">
+<form method="get" action="./indexWelcome.php">
+<label for="favcolor">Background color:</label> <br>
+  <input type="color" id="favcolor" name="favcolor"> <br>
 
-        <?php echo "<h1>Welcome $_GET[fname] $_GET[lname]</h1>" ?>
+<label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" ><br>
 
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Chiave</th>
-                    <th>Valore</ht>
-                </tr>
-            </thead>
-            <tbody>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname"><br>
 
-                <?php
-                foreach ($_GET as $k => $v) {
-                    //$v = $v ?: "NULL";
-                    echo "<tr> <td> $k </td> <td> $v </td> </tr>";
-                }
-                ?>
+  <label for="age">Age:</label> <br>
+  <input type="number" id="age" name="age"> <br>
 
-            </tbody>
-        </table>
-    </div>
+  <label for="favsport">Favourite sport:</label><br>
+  <input type="text" id="favsport" name="favsport"> <br>
+
+  <label for="email">Email:</label><br>
+  <input type="email" id="email" name="email"><br>
+
+  <label for="pwd">Password:</label><br>
+  <input type="password" id="pwd" name="pwd"><br>
+
+  <input type="submit">
+</form>
+
+
+
+
 </body>
-
 </html>
+
---
+++ esercizi/questionario/indexWelcome.php
@@ -0,0 +1,36 @@
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<?php
+    echo "<body style=\"background-color: $_GET[favcolor];\">"
+    ?>
+ 
+
+    <div class="container">
+
+        <?php echo "<h1>Welcome ".$_GET['fname']." $_GET[lname]</h1>" ?>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Chiave</th>
+                    <th>Valore</ht>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php
+                foreach ($_GET as $k => $v) {
+                    //$v = $v ?: "NULL";
+                    echo "<tr> <td> $k </td> <td> $v </td> </tr>";
+                }
+                ?>
+
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
+
+
